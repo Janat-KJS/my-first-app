8aa204fd30a2d467827c0cf454c353350964fc59
Acer <kulna>
2022-07-18 10:29:22 +0600

    init: start project

---
+++ src/components/MainContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card";
+
+const MainContent = () => {
+    return (
+        <div className="content">
+            <Card name="Iphone 12 pro" price="70000"/>
+            <Card name="Iphone 13 pro" price="80000"/>
+        </div>
+    )
+}
+
+export default MainContent;
